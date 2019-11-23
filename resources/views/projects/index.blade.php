c6f5c1cda60fb9eb9ff09f1c1c0503fdfa774ccb
DevAhmedHelmy <helmyahmed248@gmail.com>
2019-11-23 14:54:54 +0200

    --- Auto-Git Commit ---

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.app')
 @section('content')
     <header class="flex items-center mb-3 py-4">
-  
-        <a href="/projects/create" class="button is-info">Create a New Project</a>
+        <div class="flex justify-between w-full">
+            <h1 class="text-gray-500 no-underline">My Projects</h1>
+            <button href="/projects/create" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">New Project</button>
+        </div>
+        
     </header>
     
 
@@ -11,7 +14,7 @@
         <div class="w-1/3 px-4 pb-6">
             <div class="bg-white p-5 rounded shadow">
             
-                <h3 class="font-normal text-xl py-6"><a href="{{$project->path()}}">{{$project->title}}</a></h3>
+                <h3 class="font-normal text-xl py-6 -ml-5 border-solid border-l-4 border-blue-300 pl-4"><a href="{{$project->path()}}">{{$project->title}}</a></h3>
                 <div class="text-gray-500">{{str_limit($project->description)}}</div>
                     
             </div>
