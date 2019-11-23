013aed0369f3b9bfc0c4a5e5288ca586bee0b47d
DevAhmedHelmy <helmyahmed248@gmail.com>
2019-11-23 17:42:57 +0200

    --- Auto-Git Commit ---

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,13 +1,32 @@
 @extends('layouts.app')
 @section('content')
-    <div>
-        <h2>{{$project->title}}</h2>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <h1 class="text-gray-500 no-underline">My Projects</h1>
+            <button href="/projects/create" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">New Project</button>
+        </div>
+        
+    </header>
+    <main>
+        <div class="flex">
+            <div>
+                <h2 class="text-gray-500 no-underline">Tasks</h2>
+                {{-- tasks --}}
 
 
-        <p>{{$project->description}}</p>
-    </div>
-    <div>
-        <a href="/projects" class="button is-info">Go Back</a>
-    </div>
+                <h2 class="text-gray-500 no-underline">General Notes</h2>
+                <div class="card">lorem ipsum</div>
+            </div>
+                
+             <div>{{$project->description}}</div>
+    
+    
+         
+        </div>
+        <div>
+            <a href="/projects" class="button is-info">Go Back</a>
+        </div>
+    </main>
+    
     
 @endsection
