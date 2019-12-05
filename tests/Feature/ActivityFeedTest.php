d5f7ae585121894fd2d10c4563dc714c85a2605f
DevAhmedHelmy <helmyahmed248@gmail.com>
2019-12-05 09:35:24 +0200

    test

--- tests/Feature/ActivityFeedTest.php
+++ tests/Feature/ActivityFeedTest.php
@@ -16,6 +16,14 @@ class ActivityFeedTest extends TestCase
         $this->withoutExceptionHandling();
         $project = ProjectFactory::create();
         $this->assertCount(1, $project->activity); 
-        $this->assertEquals('created',$project->activity[0]->description)
+        $this->assertEquals('created',$project->activity[0]->description);
+    }
+    /** @test */
+    function updating_a_project_generates_activity()
+    {
+        $project = ProjectFactory::create();
+        $project->update(['title' => 'chanaged']);
+        $this->assertCount(2, $project->activity);
+        $this->assertEquals('updated',$project->activity->last()->description);
     }
 }
